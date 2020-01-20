c6676875c19f1a74ebe9da32b1d4793c3cbc3be8
alumno <alumno@E3MAESTRO>
2020-01-20 07:52:29 -0700

    info de glew

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -36,13 +36,28 @@ int main()
 	}
 	//Establecemos la ventana como contexto
 	glfwMakeContextCurrent(window);
+
+	//Una vez establecido el contexto
+	//Se activan las funciones "modernas" (gpu)
+	glewExperimental = true;
+
+	GLenum errores = glewInit();
+	if (errores != GLEW_OK) {
+		glewGetErrorString(errores);
+	}
+
+	const GLubyte* versionGL =
+		glGetString(GL_VERSION);
+	cout << "Version OpenGL: " << versionGL;
+
+
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
 		//Establecer region de dibujo
 		glViewport(0, 0, 800, 600);
 		//Establecemos el color de borrado
 		//Valores RGBA
-		glClearColor(1, 0.2, 0.5, 1);
+		glClearColor(1, 0.8, 0, 1);
 		//Borrar!
 		glClear(GL_COLOR_BUFFER_BIT | 
 			GL_DEPTH_BUFFER_BIT);
